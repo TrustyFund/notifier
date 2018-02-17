d887d153ca31fada5342372930917d8456c21bfd
Anton <anlopan@gmail.com>
2018-02-17 05:39:52 +0300

    Fix keys (it works after api connect)

--- test2.js
+++ test2.js
@@ -1,33 +1,15 @@
 const {Apis} = require('bitsharesjs-ws');
 const {PrivateKey, key, Aes} = require('bitsharesjs');
 
-const brainkey = "glink omental webless pschent knopper brumous scarry were wasting isopod raper barbas maco kirn tegua mitome";
-const password = '111111111';
+const brainkey = 'glink omental webless pschent knopper brumous scarry were wasting isopod raper barbas maco kirn tegua mitome';
 
-const passwordAes = Aes.fromSeed(password);
-const encryptionBuffer = key.get_random_key().toBuffer();
-const encryptionKey = passwordAes.encryptToHex(encryptionBuffer);
-const aesPrivate = Aes.fromSeed(encryptionBuffer);
-
-const normalizedBrainkey = key.normalize_brainKey(brainkey);
-// const brainkeyPrivate = PrivateKey.fromSeed(normalizedBrainkey);
-const encryptedBrainkey = aesPrivate.encryptToHex(normalizedBrainkey);
-const passwordPrivate = PrivateKey.fromSeed(password);
-const passwordPubkey = passwordPrivate.toPublicKey().toPublicKeyString();
-
-// getting user id
-const ownerKeyIndex = 1;
-const ownerKey = key.get_brainPrivateKey(normalizedBrainkey, ownerKeyIndex);
-const ownerPubkey = ownerKey.toPublicKey().toPublicKeyString();
-
-
-console.log("\nPrivate key:",ownerKey.toWif());
-console.log("Public key :", ownerPubkey, "\n");
-
-/*
 Apis.instance('wss://dex.rnglab.org/ws', true).init_promise.then(() => {
+	const normalizedBrainkey = key.normalize_brainKey(brainkey);
+	const ownerKey = key.get_brainPrivateKey(normalizedBrainkey, 1);
+	const ownerPubkey = ownerKey.toPublicKey().toPublicKeyString();
+
 	Apis.instance().db_api().exec('get_key_references', [[ownerPubkey]])
     .then(([[userId]]) => {
       console.log(userId);
     });
-});*/
+});
