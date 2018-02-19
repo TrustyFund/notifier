0eec6a07de45ec95c173be547748507400617c51
ololokiras <bellendirr@yandex.ru>
2018-02-19 13:57:45 +0800

    repair Utils

--- src/Utils.js
+++ src/Utils.js
@@ -1,16 +1,16 @@
 const fs = require('fs');
 
-getRealCost(amount, precision) {
+function getRealCost(amount, precision) {
   return Math.abs(amount / (10 ** precision)).toFixed(9);
 }
 
-writeToFile(data) {
+function writeToFile(data) {
   fs.appendFile('block_log', data + '\n\n', (error) => {
     if (error) throw error;
   })
 }
 
-formatPrice(price,base,quote){
+function formatPrice(price, base, quote) {
   let precision_diff = base.precision - quote.precision;
   price = (precision_diff > 0) ? price / Math.pow(10,precision_diff) : price * Math.pow(10,-precision_diff);    
   return Math.abs(price).toFixed(7);
