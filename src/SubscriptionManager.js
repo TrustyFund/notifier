c8965fe25f7c0106d0825fc4cc13ab3f8894e143
Anton <anlopan@gmail.com>
2018-02-19 07:15:43 +0300

    Get subscriptions from history

--- src/SubscriptionManager.js
+++ src/SubscriptionManager.js
@@ -3,32 +3,56 @@ const { key, Aes } = require('bitsharesjs');
 
 
 class SubscriptionManager {
-  constructor(brainkey) {
-    this.brainkey = brainkey;
-  }
-
-  setServiceUserId(userId) {
-    this.serviceUserId = userId;
-  }
-
-  async getServiceUserId() {
-    const normalizedBrainkey = key.normalize_brainKey(this.brainkey);
+  async setServiceUser(brainkey) {
+    const normalizedBrainkey = key.normalize_brainKey(brainkey);
     const ownerKey = key.get_brainPrivateKey(normalizedBrainkey, 1);
     const ownerPubkey = ownerKey.toPublicKey().toPublicKeyString();
     const [[userId]] = await Apis.instance().db_api().exec('get_key_references', [[ownerPubkey]]);
-    this.setServiceUserId(userId);
+    this.serviceUserId = userId;
+    this.ownerKey = ownerKey;
     return userId;
   }
 
-  async getActiveSubscriptions() {
+  async getActiveSubscriptions(types) {
     const allHistory = await this.getAllHistory();
-    return allHistory;
+    let subscribedUsers = {};
+    let unsubscribedUsers = {};
+    let destinations = {};
+
+    types.forEach((item) => {
+      subscribedUsers[item] = [];
+      unsubscribedUsers[item] = [];
+      destinations[item] = {};
+    })
+
+    allHistory.forEach((item) => {
+      if (item.op[1].memo){
+        const message = decryptMemo(this.ownerKey, item.op[1].memo);
+        const clientId = item.op[1].from;
+        let [action,...data] = message.split(':');
+        if (action && data) {
+          // Need to join rightside because android token has same devider
+          data = data.join('');
+          if (action === 'stop' && types.includes(data)) {
+            unsubscribedUsers[data].push(clientId);
+          } 
+          if (types.includes(action) && !unsubscribedUsers[action].includes(clientId) && !subscribedUsers[action].includes(clientId)){
+            subscribedUsers[action].push(clientId);
+            destinations[action][clientId] = data;
+          }
+        }
+      }
+    });
+    return destinations;
   }
 
   async getAllHistory() {
     const allHistory = [];
+
+    // Because of bitshares fetching from new to old, we need to ensure that no messages missed
     let fromId = '1.11.999999999';
-    // We need await in loop becaouse each request depends on previous
+
+    // We need await in loop because each request depends on previous
     // eslint-disable-next-line no-await-in-loop
     for (;;) {
       const history = await this.getSegmentHistory(fromId);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 const { Apis } = require('bitsharesjs-ws');
 const NotificationSender = require('./NotificationSender');
 const OperationListener = require('./OperationListener');
+const SubscriptionManager = require('./SubscriptionManager');
 const config = require('../config');
 
 const notificationSender = new NotificationSender('./trusty-informator-firebase-adminsdk-808sd-9702018d1f.json', config.emailTransport);
 
-Apis.instance('wss://dex.rnglab.org/ws', true).init_promise.then(() => {
+Apis.instance('wss://openledger.hk/ws', true).init_promise.then(async () => {
+  const subscriptionManager = new SubscriptionManager();
+  const serviceUserId = await subscriptionManager.setServiceUser(config.serviceUserBrainkey);
+  const activeSubscriptions = await subscriptionManager.getActiveSubscriptions(['email']);
+  console.log("Subscriptions", activeSubscriptions);
+  //console.log("Active Subscriptions", activeSubscriptions);
+  
+
+/*
   const emails = { '1.2.512210': 'anlopan@gmail.com' };
   const operationListener = new OperationListener(['1.2.512210']);
   operationListener.setEventCallback((notification) => {
     const { userId, message } = notification;
     notificationSender.sendMessage(message, emails[userId], 'email');
-  });
+  });*/
 });
 
